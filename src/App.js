f695ee55121de45f6e9dfc66cb616c6322b6aa53
ard <aaron@ard.ninja>
2017-10-26 23:20:31 -0500

    add image test

--- src/App.js
+++ src/App.js
@@ -12,11 +12,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <RaisedButton>Test</RaisedButton>
-        </p>
         <div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+        <div>
+          <RaisedButton>Test</RaisedButton>
+
           <ProductGrid />
         </div>
       </div>
--- src/components/ProductTile/index.js
+++ src/components/ProductTile/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Paper from 'material-ui/Paper'
+import './index.css'
 
 const ProductTile = (props) => {
   return (
@@ -7,6 +8,10 @@ const ProductTile = (props) => {
       <Paper zDepth={1}>
         <span className='ProductName'>{ props.product.name }</span>
         <span className='ProductPrice'>${ props.product.price }</span>
+        <img className='ProductImage'
+          src={ props.product.image }
+          alt={props.product.name}
+        />
       </Paper>
     </div>
   )
